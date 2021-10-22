ca3fb5e99b17554836463e9de06539c9438783cb
Konrad Erlendsson <konrad@menandmice.com>
2021-10-22 12:46:37 +0000

    append to footer text in Login view

--- src/login/views/Login.tsx
+++ src/login/views/Login.tsx
@@ -24,7 +24,7 @@ const Login = () => (
         <button type="submit">{t`Login`}</button>
       </div>
     </form>
-    <footer>{t`Version ${version}`}</footer>
+    <footer>{t`Version ${version} -- appended text`}</footer>
   </div>
 );
 
